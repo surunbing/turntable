649e415e44e6f41621e55f7dfe8c681fa9741d2b
Runbing Su <surunbing@momenta.ai>
2018-11-06 19:10:44 +0800

    change max to a list of constraint .

--- nonlcon1.m
+++ nonlcon1.m
@@ -5,7 +5,7 @@ function [c,ceq] = nonlcon1(x, data, series, P, bandwidth, nCon, nCRp)
 mag = mag + data.mag;
 phi = phi +data.phi;
 nCount = nCon;
-c = zeros(nCon + 3, 1);
+c = zeros(nCon + 2 + nCRp, 1);
 for i = 1 : nCon
     c(i) = - phi(i) - 170;
 end
@@ -19,6 +19,8 @@ complex_bode = 10 .^ (Mag ./ 20) .* complex(cos(Phi ./ 180 .* pi), sin(Phi ./ 18
 complex_bode = complex_bode ./ (1 + complex_bode);
 %转成bode形式
 Mag = log10(abs(complex_bode)) * 20;
-Mag_max = max(Mag);
-c(nCount + 3) = Mag_max - 6;
+%Mag_max = max(Mag);
+for i = 1 : nCRp
+    c(nCount + 2 + i) = Mag(nCon + 2 + i) - 6;
+end
 ceq = [];
