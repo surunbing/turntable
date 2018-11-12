eba310c6b63e66b270c2f0118df64e6134e1f8b1
Runbing Su <surunbing@momenta.ai>
2018-11-12 22:48:40 +0800

    Add check.

---
+++ wave-check/CLIndic_check.m
@@ -0,0 +1,54 @@
+function [data_check] = CLIndic_check(data, bandwidth, option)
+% 检查双十特性 开环检查或闭环检查
+ncount = round(bandwidth / 2 / pi);
+data_check.mag = zeros(ncount, 1);
+data_check.phi = zeros(ncount, 1);
+data_check.mag11_reg = zeros(ncount, 1);
+data_check.mag09_reg = zeros(ncount, 1);
+data_check.phips_reg = zeros(ncount, 1);
+data_check.phing_reg = zeros(ncount, 1);
+if strcmp(option.type, 'close-loop') == 1
+    for i = 1 : ncount
+       if data.mag(i) > 0.9 && data.mag(i) < 1.1
+           data_check.mag(i) = 1;
+       else
+           data_check.mag(i) = 0;
+       end
+       if abs(data.phi(i)) < 10
+           data_check.phi(i) = 1;
+       else
+           data_check.phi(i) = 0;
+       end
+       %% 计算差值
+       data.mag11_reg(i) = 1.1 - data.mag(i);
+       data.mag09_reg(i) = data.mag(i) - 0.9;
+       data.phips_reg(i) = 10 - data.phi(i);
+       data.phing_reg(i) = data.phi(i) + 10;
+    end
+elseif strcmp(option.type, 'open-loop') == 1
+    %% 由开环计算闭环
+    data_close = data;
+    close_complex = data.mag .* complex(cos(data.phi / 180 * pi), sin(data.phi / 180 * pi));
+    close_complex = close_complex / (1 + close_complex);
+    data_close.mag = abs(close_complex);
+    data_close.phi = angle(close_complex) / pi * 180);
+    for i = 1 : ncount
+        if data_close.mag(i) > 0.9 && data_close.mag(i) < 1.1
+           data_check.mag(i) = 1;
+       else
+           data_check.mag(i) = 0;
+       end
+       if abs(data_close.phi(i)) < 10
+           data_check.phi(i) = 1;
+       else
+           data_check.phi(i) = 0;
+       end
+       %% 计算差值
+       data.mag11_reg(i) = 1.1 - data_close.mag(i);
+       data.mag09_reg(i) = data_close.mag(i) - 0.9;
+       data.phips_reg(i) = 10 - data_close.phi(i);
+       data.phing_reg(i) = data_close.phi(i) + 10;
+    end
+end
+end
+
---
+++ wave-check/Rp_check.m
@@ -0,0 +1,28 @@
+function [fre, Rp, flag] = Rp_check(data, bandwidth, ratio, Rp_max, option)
+%   ·µ»Ø±Õ»·Ð³Õñ·å, db
+if strcmp(option.type, 'transfer') == 1
+    [mag, phi, frequence] = bode(data);
+    ncount = length(frequence);
+    Mag = zeros(ncount, 1);
+    Phi = zeros(ncount, 1);
+    for i = 1 : ncount
+        Mag(i) = mag(1, 1, i);
+        Phi(i) = phi(1, 1, i);
+    end
+    Rp = max(Mag);
+    num = find(Mag == Rp);
+    fre = frequence(num(1));
+    Rp = 20 * log10(Rp);
+elseif strcmp(option.type, 'discrete') == 1
+    Rp = max(data.mag);
+    num = find(data.mag == Mag_max);
+    fre = data.fre(num(1)); 
+    Rp = 20 * log10(Rp);
+end
+if Rp > Rp_max
+    flag = 0;
+elseif Rp <= Rp_max
+    flag = 1;
+end
+end
+
